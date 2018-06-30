6b177659b2bffff10942b5e60c887a61bf6e47de
thando <tzone85@gmail.com>
2018-06-30 17:08:39 +0200

    created the relationship between the reviews and the product. Used Tinker a little bit to see if the data was being generated properly

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function products(){
+
+        return $this->belongsTo(Product::class);
+
+    }
 }
